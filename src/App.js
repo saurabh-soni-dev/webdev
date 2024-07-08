92874f1ec72ce87948501004ebe13d2baff7f370
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-07-08 09:06:58 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Routes from "./routes/routes";
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes />
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default App;
